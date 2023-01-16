9806776be2a0b19f76ae80781f576277fe227052
waqas1412 <muhammad.waqas1412@gmail.com>
2023-01-16 17:57:07 +0500

    Recipe Setuped

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'Test Description',
+      'https://www.shutterstock.com/image-photo/notepad-your-recipe-herbs-spices-600w-370298699.jpg'
+    ),
+  ];
 }
